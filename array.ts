5e4f5d007298232adb9af60f40f896ecce5c7088
LeNgocBao145 <himagito@gmail.com>
2025-10-23 19:33:47 +0700

    feat: array in Typescript

---
+++ array.ts
@@ -0,0 +1,17 @@
+let ratings: number[] = [5, 4, 3, 5];
+let petNames: string[] = ["Miu", "Cún", "Chíp"];
+
+let ratings2: Array<number> = [5, 4, 3, 5];
+let petNames2: Array<string> = ["Miu", "Cún", "Chíp"];
+
+let matrix: number[][] = [
+  [1, 2],
+  [0, 1],
+  [2, 4],
+];
+
+let matrix2: Array<Array<number>> = [
+  [1, 2],
+  [0, 1],
+  [2, 4],
+];
