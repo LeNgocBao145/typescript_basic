c41e6ba60cb5a8bbedfbd1b4044fc53ab08f07b9
LeNgocBao145 <himagito@gmail.com>
2025-10-23 20:19:26 +0700

    feat: types alias

---
+++ typesAlias.ts
@@ -0,0 +1,35 @@
+// Notice these types aliases only exist at compile time
+// Can't use typeof or instanceof to check these types at runtime
+
+type Customer = {
+  id: number;
+  name: string;
+};
+
+type UserResponse = {
+  success: boolean;
+  message: string;
+};
+
+function greetCustomer(customer: Customer): UserResponse {
+  return {
+    success: true,
+    message: `Xin chào ${customer.name}, cảm ơn bạn đã đến với cửa hàng!`,
+  };
+}
+
+type SupportFunction = (customer: Customer) => UserResponse;
+
+const greetCustomer2: SupportFunction = (customer) => {
+  return {
+    success: true,
+    message: `Xin chào ${customer.name}, cảm ơn bạn đã đến với cửa hàng!`,
+  };
+};
+
+const farewellCustomer: SupportFunction = (customer) => {
+  return {
+    success: true,
+    message: `Hẹn gặp lại ${customer.name}, chúc bạn một ngày vui vẻ!`,
+  };
+};
