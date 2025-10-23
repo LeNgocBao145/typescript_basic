a94dabb64229287e308055177a3e0783a54ba67c
LeNgocBao145 <himagito@gmail.com>
2025-10-23 20:21:39 +0700

    feat: import types alias from other files, modules

---
+++ typesAliasImport.ts
@@ -0,0 +1,7 @@
+// Importing types from another module
+
+// Method 1: import entire module
+// import type { UserResponse, SupportFunction } from "./typesAlias.js";
+
+// Method 2: import specific types
+import { type UserResponse, type SupportFunction } from "./typesAlias.js";
